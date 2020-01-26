4e9539f9b4e3ae1d28efef2dad62b245c87ef511
Christian Jimenez <christianjimenez0123@gmail.com>
2020-01-26 20:57:47 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+
+  get 'new/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,6 +17,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :coupons, only: [:index, :show,  :create, :new, :edit]
 
   # Example resource route with options:
   #   resources :products do
